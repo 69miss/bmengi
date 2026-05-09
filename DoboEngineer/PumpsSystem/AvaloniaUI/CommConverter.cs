17c7cda9645218320f27b431762fe440f2e0f277
hll <he_liangliang@outlook.com>
2026-05-09 15:31:30 +0800

    添加转换类

--- DoboEngineer/PumpsSystem/AvaloniaUI/CommConverter.cs
+++ DoboEngineer/PumpsSystem/AvaloniaUI/CommConverter.cs
@@ -1,21 +1,23 @@
-﻿using Avalonia.Data.Converters;
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
 using Dobo.Appl.Utility;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 
 namespace PumpsSystem.AvaloniaUI;
 
-    /// <summary>
-    /// Bool 转字符串转换器
-    /// </summary>
-    public class CommConverter : IValueConverter
-    {
+/// <summary>
+/// Bool 转字符串转换器
+/// </summary>
+public class CommConverter : IValueConverter
+{
 
     /// <summary>
     /// Bool → 字符串（正向转换）
@@ -23,28 +25,34 @@ namespace PumpsSystem.AvaloniaUI;
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         var paramArr = FastSerialize.Instance.FromSeparator<string>(parameter + "");
-        if (value is int intVal)
+        var valueStr = value + "";
+        if (value is IConvertible intVal&& (targetType == typeof(bool)|| Nullable.GetUnderlyingType(targetType) == typeof(bool)))
         {
-            if (targetType == typeof(bool))
-            {
-                
-            }
-
+                var trueVal = paramArr.Length > 0 ? paramArr[0] : "1";
+                var falseVal = paramArr.Length > 1 ? paramArr[1] : "0";
+                return !string.IsNullOrEmpty(trueVal) ? trueVal.Equals(valueStr) : !falseVal.Equals(valueStr);
         }
-        return "";
+        return null;
     }
 
-        /// <summary>
-        /// 字符串 → Bool（反向转换，如需要双向绑定才实现）
-        /// </summary>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value == null)
-                return false;
+    /// <summary>
+    /// 字符串 → Bool（反向转换，如需要双向绑定才实现）
+    /// </summary>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return null;
+        var paramArr = FastSerialize.Instance.FromSeparator<string>(parameter + "");
+        var valueStr = value + "";
 
-            string strValue = value.ToString().Trim();
-            // 根据文本反向转换为 bool（示例："启用"→true，其他→false）
-            return strValue == "启用" || strValue == "是" || strValue == "√";
+
+        if (value is bool boolVal && (targetType == typeof(int)|| targetType == typeof(short)))
+        {
+            if (boolVal)
+                return paramArr.Length > 0 && int.TryParse(paramArr[0], out int trueVal) ? trueVal : null;
+            return paramArr.Length > 1 && int.TryParse(paramArr[1], out int falseVal) ? falseVal : null;
         }
+        return null;
     }
+}
 
